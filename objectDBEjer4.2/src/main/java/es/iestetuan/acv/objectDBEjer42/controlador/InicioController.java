612f01fe6252e27ab62cb4613d57ad321efe1e52
Desun92 <adri.villa92@gmail.com>
2022-02-07 09:53:13 +0100

    Lista Colores y Lineas

--- objectDBEjer4.2/src/main/java/es/iestetuan/acv/objectDBEjer42/controlador/InicioController.java
+++ objectDBEjer4.2/src/main/java/es/iestetuan/acv/objectDBEjer42/controlador/InicioController.java
@@ -1,5 +1,14 @@
 package es.iestetuan.acv.objectDBEjer42.controlador;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class InicioController {
+	
+	@RequestMapping("/")
+	public String iniciarConPlantilla(){
+		return "inicio";
+	}
 
 }
